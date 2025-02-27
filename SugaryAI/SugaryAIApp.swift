c8e7b973c17f89bec84f5d1bfcac89a9e59d645c
ShathaMK <shathamukhaild@gmail.com>
2025-02-27 12:42:22 +0300

    Initial Commit

---
+++ SugaryAI/SugaryAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SugaryAIApp.swift
+//  SugaryAI
+//
+//  Created by Shatha Almukhaild on 28/08/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct SugaryAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
